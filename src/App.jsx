a0de87e2dc97304a18eb408561e217e80ca8243b
Ayush Katare <katareayush2005@gmail.com>
2025-03-11 02:50:04 +0530

    landing completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { ClerkProvider } from '@clerk/clerk-react'
 import './App.css'
-import Footer from './components/footer'
+import Landing from './components/Landing'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 function App() {
 
+  if (!PUBLISHABLE_KEY) {
+    throw new Error("Missing Publishable Key")
+  }
 
   return (
-   <Footer />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <Landing />
+    </ClerkProvider>
   )
 }
 
